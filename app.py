308c15bb399f5b220f0381a5ef8989eb1a9078b3
atandon9 <anirudhx.tandon@intel.com>
2021-09-30 15:58:09 +0530

    Add files via upload
    new additions

---
+++ app.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+from time import gmtime, strftime
+
+def main():
+	# Configure the logging system
+	logging.basicConfig(filename ='/opt/results/app-log.txt', level = logging.INFO)
+	
+	# Variables (to make the calls that follow work)
+	hostname = 'www.python.org'
+	item = 'spam'
+	filename = 'data.csv'
+	mode = 'r'
+	
+	# Example logging calls (insert into your program)
+	print("hello world")
+	logging.info("hello world")
+	logging.info(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	
+if __name__ == '__main__':
+	main()
+sys.exit
